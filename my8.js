6b425cf714afd2aef2cb857cbec9225dc45c9a2f
Rahmanism <s.mostafa.rahmani@gmail.com>
2019-07-07 20:38:28 +0430

    Enums!

--- my8.js
+++ my8.js
@@ -1,9 +1,31 @@
+var Gender;
+(function (Gender) {
+    Gender[Gender["Male"] = 0] = "Male";
+    Gender[Gender["Female"] = 1] = "Female";
+    Gender[Gender["Other"] = 2] = "Other";
+})(Gender || (Gender = {}));
 var mostafa = {
     name: "Mostafa",
     family: "Rahmani",
-    age: 38
+    age: 38,
+    gender: Gender.Male
 };
+console.log(mostafa);
 function register(person) {
     console.log(person.name + " " + person.family);
 }
 register(mostafa);
+var Days;
+(function (Days) {
+    Days[Days["Saturday"] = 1] = "Saturday";
+    Days[Days["Sunday"] = 3] = "Sunday";
+    Days[Days["Monday"] = 42] = "Monday";
+    Days[Days["Tuesday"] = 33] = "Tuesday";
+    Days[Days["Wednesday"] = 4] = "Wednesday";
+    Days[Days["Thursday"] = 7] = "Thursday";
+    Days[Days["Friday"] = 22] = "Friday";
+})(Days || (Days = {}));
+var day = Days.Sunday;
+console.log(day);
+day = Days.Tuesday;
+console.log(day);
--- my8.ts
+++ my8.ts
@@ -3,13 +3,23 @@ interface IPerseon {
     family: string;
     age: number;
     email?: string;
+    gender: Gender;
+}
+
+enum Gender {
+    Male,
+    Female,
+    Other
 }
 
 var mostafa: IPerseon = {
     name: "Mostafa",
     family: "Rahmani",
-    age: 38
+    age: 38,
+    gender: Gender.Male
 }
+
+console.log(mostafa);
 
 function register(person: IPerseon): void {
     console.log(person.name + " " + person.family);
@@ -23,3 +33,20 @@ interface IPerseonService {
     getAll(): IPerseon[];
     getById(personId: number): IPerseon;
 }
+
+
+
+enum Days {
+    Saturday = 1,
+    Sunday = 3,
+    Monday = 42,
+    Tuesday = 33,
+    Wednesday = 4,
+    Thursday = 7,
+    Friday = 22
+}
+
+var day = Days.Sunday;
+console.log(day);
+day = Days.Tuesday;
+console.log(day);
