13d4165b067e01880bfd46ff427b4635007d59bd
Rahmanism <s.mostafa.rahmani@gmail.com>
2019-10-19 20:41:50 +0330

    Inheritance!

--- todo.js
+++ todo.js
@@ -1,16 +1,51 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var TodoState;
 (function (TodoState) {
     TodoState[TodoState["Active"] = 0] = "Active";
     TodoState[TodoState["Completed"] = 1] = "Completed";
     TodoState[TodoState["Deleted"] = 2] = "Deleted";
 })(TodoState || (TodoState = {}));
-var SmartTodo = /** @class */ (function () {
-    function SmartTodo(name, state) {
+// class SmartTodo {
+//     name: string;
+//     _state: TodoState;
+//     constructor(name: string, state: TodoState = TodoState.Active) {
+//         this.name = name;
+//         this._state = state;
+//     }
+//     get state() {
+//         return this._state;
+//     }
+//     set state(newState: TodoState) {
+//         if (newState == TodoState.Completed) {
+//             let canBeCompleted = 
+//                 this._state == TodoState.Active ||
+//                 this._state == TodoState.Deleted;
+//             if (!canBeCompleted) {
+//                 throw new Error("The completed item can't be completed again!");
+//             }
+//         }
+//         this._state = newState;
+//     }
+// }
+var Todo = /** @class */ (function () {
+    function Todo(name, state) {
         if (state === void 0) { state = TodoState.Active; }
         this.name = name;
         this._state = state;
     }
-    Object.defineProperty(SmartTodo.prototype, "state", {
+    Object.defineProperty(Todo.prototype, "state", {
         get: function () {
             return this._state;
         },
@@ -27,10 +62,37 @@ var SmartTodo = /** @class */ (function () {
         enumerable: true,
         configurable: true
     });
-    return SmartTodo;
+    return Todo;
 }());
-var todo13 = new SmartTodo("Test");
-console.log(todo13.state);
-todo13.state = TodoState.Completed;
-console.log(todo13.state);
-todo13.state = TodoState.Completed;
+var TodoStateChanger = /** @class */ (function () {
+    function TodoStateChanger(newState) {
+        this.newState = newState;
+    }
+    TodoStateChanger.prototype.canChangeState = function (todo) {
+        return !!todo;
+    };
+    TodoStateChanger.prototype.changeState = function (todo) {
+        if (this.canChangeState(todo)) {
+            todo.state = this.newState;
+        }
+        return todo;
+    };
+    return TodoStateChanger;
+}());
+var ChangeTodoToComplete = /** @class */ (function (_super) {
+    __extends(ChangeTodoToComplete, _super);
+    function ChangeTodoToComplete() {
+        return _super.call(this, TodoState.Completed) || this;
+    }
+    ChangeTodoToComplete.prototype.canChangeState = function (todo) {
+        return _super.prototype.canChangeState.call(this, todo) &&
+            (todo.state == TodoState.Active || todo.state == TodoState.Deleted);
+    };
+    return ChangeTodoToComplete;
+}(TodoStateChanger));
+var todoCompleteTest = new ChangeTodoToComplete();
+// let todo13 = new SmartTodo("Test");
+// console.log(todo13.state);
+// todo13.state = TodoState.Completed;
+// console.log(todo13.state);
+// todo13.state = TodoState.Completed;
--- todo.ts
+++ todo.ts
@@ -4,12 +4,38 @@ interface ITodo {
 }
 
 enum TodoState {
-    Active, 
+    Active,
     Completed,
     Deleted
 }
 
-class SmartTodo implements ITodo {
+// class SmartTodo {
+//     name: string;
+//     _state: TodoState;
+
+//     constructor(name: string, state: TodoState = TodoState.Active) {
+//         this.name = name;
+//         this._state = state;
+//     }
+
+//     get state() {
+//         return this._state;
+//     }
+
+//     set state(newState: TodoState) {
+//         if (newState == TodoState.Completed) {
+//             let canBeCompleted = 
+//                 this._state == TodoState.Active ||
+//                 this._state == TodoState.Deleted;
+//             if (!canBeCompleted) {
+//                 throw new Error("The completed item can't be completed again!");
+//             }
+//         }
+//         this._state = newState;
+//     }
+// }
+
+class Todo implements ITodo {
     name: string;
     _state: TodoState;
 
@@ -24,7 +50,7 @@ class SmartTodo implements ITodo {
 
     set state(newState: TodoState) {
         if (newState == TodoState.Completed) {
-            let canBeCompleted = 
+            let canBeCompleted =
                 this._state == TodoState.Active ||
                 this._state == TodoState.Deleted;
             if (!canBeCompleted) {
@@ -35,9 +61,38 @@ class SmartTodo implements ITodo {
     }
 }
 
-let todo13 = new SmartTodo("Test");
-console.log(todo13.state);
-todo13.state = TodoState.Completed;
-console.log(todo13.state);
-todo13.state = TodoState.Completed;
+class TodoStateChanger {
+    constructor(private newState: TodoState) { }
+
+    canChangeState(todo: Todo): boolean {
+        return !!todo;
+    }
+
+    changeState(todo: Todo): Todo {
+        if (this.canChangeState(todo)) {
+            todo.state = this.newState;
+        }
+
+        return todo;
+    }
+}
+
+class ChangeTodoToComplete extends TodoStateChanger {
+    constructor() {
+        super(TodoState.Completed);
+    }
+
+    canChangeState(todo: Todo): boolean {
+        return super.canChangeState(todo) &&
+            (todo.state == TodoState.Active || todo.state == TodoState.Deleted);
+    }
+}
+
+let todoCompleteTest = new ChangeTodoToComplete();
+
+// let todo13 = new SmartTodo("Test");
+// console.log(todo13.state);
+// todo13.state = TodoState.Completed;
+// console.log(todo13.state);
+// todo13.state = TodoState.Completed;
 
